dc91932de7c3189335af96b16eea4baa96ac9f3b
taylorkarras <taylorkarras@gmail.com>
2016-11-25 14:19:43 -0800

    v1.1 update

--- includes/console/search/postsearchresults.php
+++ includes/console/search/postsearchresults.php
@@ -18,7 +18,24 @@ echo '<h1 style="display:table; margin:0;">No search terms entered, please enter
 $haserror = true;
   }
 //Category
-  if (preg_match('/^category:.*/', $url) or preg_match('/, category:.*/', $url)){
+if (preg_match('/category:"[^"]+"/im', $url) or preg_match('/, category:"[^"]+"/im', $url)){
+preg_match_all ('/category:"[^"]+"/im', $url, $category2);
+if(empty($category2[0])){
+	echo consolemenu();
+echo "<div id='page'>
+<div class='center'>There are "; echo $retrive->numberofposts(); echo " posts on this blog.</div>
+<a class='nounderline' href='/console/posts/new' alt='Create New Post' title='Create New Post'><div class='createnewpost'>Create New Post</div></a>";
+echo dbsearchbar();
+	echo '<h1 style="display:table; margin:0;">No search terms entered, please enter search terms above.</h1>';
+	$haserror = true;
+  } else {
+$category3 = implode(':',$category2[0]);
+$replace = array('category:', ',');
+$category4 = str_replace($replace, '', $category3);
+$category5 = str_replace('"', '', $category4);
+$category = " AND content_category = '".$category5."' ";
+  }
+  } else if (preg_match('/^category:.*/', $url) or preg_match('/, category:.*/', $url)){
 preg_match_all ('/category:\S+/im', $url, $category2);
 if(empty($category2[0])){
 	echo consolemenu();
@@ -35,7 +52,6 @@ $category4 = str_replace($replace, '', $category3);
 $category = " AND content_category = '".$category4."' ";
   }
   }
-
 //Tags
 if (preg_match('/^tag:.*/', $url) or preg_match('/, tag:.*/', $url)){
 preg_match_all ('/tag:"[^"]+"/im', $url, $singletag2);
@@ -51,14 +67,11 @@ echo dbsearchbar();
 $singletag3 = implode(':',$singletag2[0]);
 $replace = array('tag:', ',', '"');
 $singletag4 = str_replace($replace, '', $singletag3);
-$tags = " AND content_tags LIKE '%".$singletag4."%' ";
+$tags = " AND LOWER(content_tags) LIKE LOWER('%".$singletag5."%') ";
   }
-}
-
-  if (preg_match('/^tags:.*/', $url) or preg_match('/, tags:.*/', $url)){
-preg_match_all ('/tags:"[^"]+"/im', $url, $tags2);
-
-if(empty($tags2[0])){
+}  else if (preg_match('/^tags:.*/', $url) or preg_match('/, tags:.*/', $url)){
+preg_match_all ('/tags:.*/im', $url, $tags2);
+if(empty($singletag2[0])){
 	echo consolemenu();
 echo "<div id='page'>
 <div class='center'>There are "; echo $retrive->numberofposts(); echo " posts on this blog.</div>
@@ -69,19 +82,28 @@ echo dbsearchbar();
   } else {
 $tags3 = implode('%, %',$tags2[0]);
 $tags4 = str_replace('tags:', '', $tags3);
+if (preg_match('/^tag:"[^"]+"/im', $url) or preg_match('/, tag:"[^"]+"/im', $url)){
 $tags5 = explode(',', $tags4);
 $tags6 = implode("%, %", $tags5);
 $tags7 = trim($tags6);
 $replace = array('"', '% ');
 $replace2 = array('', '%');
 $tags8 = str_replace($replace, $replace2, $tags7);
-$tags = " AND content_tags LIKE ('%".$tags8."%')";
+$tags = " AND LOWER(content_tags) LIKE LOWER('%".$tags8."%')";
+} else {
+$replace = array('"', '% ');
+$replace2 = array('', '%');
+$replace3 = array('/, category:.*/im', '/, tags:.*/im', '/, author:.*/im', '/, date:.*/im');
+$tags5 = str_replace($replace, $replace2, $tags4);
+$tags6 = preg_replace($replace3, '', $tags5);
+$tags = " AND LOWER(content_tags) LIKE LOWER('%".$tags6."%')";
+}
   }
   }
   
   //Date
 if (preg_match('/^date:.*/', $url) or preg_match('/, date:.*/', $url)){
-preg_match_all ('/date:\S+/', $url, $date2);
+preg_match_all ('/date:.+/', $url, $date2);
 if(empty($date2[0])){
 	echo consolemenu();
 echo "<div id='page'>
@@ -90,16 +112,23 @@ echo "<div id='page'>
 echo dbsearchbar();
 	echo '<h1 style="display:table; margin:0;">No search terms entered, please enter search terms above.</h1>';
 	$haserror = true;
-  } else {
+  }  else {
 $date3 = implode(':',$date2[0]);
 $replace = array('date:', ',');
 $date4 = str_replace($replace, '', $date3);
-if (!preg_match("/^(\d{2})\/(\d{2})\/(\d{4})$/", $date4)){
+if (preg_match("/^\d{4}$/", $date4)){
+        $date = " AND content_date BETWEEN '".$date4."-01-01' AND '".$date4."-12-31'";
+} else if (preg_match('/^"[a-zA-Z0-9 ]+\d{4}"$/im', $date4)){
+	$date5 = str_replace('"', "", $date4);
+	$date6 = date('m/Y', strtotime($date5));
+	$date7 = explode('/', $date6);
+	$date = " AND content_date BETWEEN '".$date7[1]."-".$date7[0]."-01' AND '".$date7[1]."-".$date7[0]."-31'";
+} else if (!preg_match("/^(\d{2})\/(\d{2})\/(\d{4})$/", $date4)){
 	echo '<h1 style="display:table; margin:0;">Please enter the date in the MM/DD/YYYY format.</h1>';
 	$haserror = true;
 } else {
 	$date5 = explode('/', $date4);
-	$date = " AND content_date = '".$date5[2].'/'.$date5[1].'/'.$date5[0]."%' ";
+	$date = " AND content_date LIKE '%".$date5[2].'-'.$date5[0].'-'.$date5[1]."%' ";
 }
   }
 }
@@ -107,6 +136,33 @@ if (!preg_match("/^(\d{2})\/(\d{2})\/(\d{4})$/", $date4)){
 //Author
   if (preg_match('/^author:.*/', $url) or preg_match('/, author:.*/', $url)){
 preg_match_all ('/author:"[^"]+"/im', $url, $author2);
+if(empty($author2[0])){
+	echo consolemenu();
+echo "<div id='page'>
+<div class='center'>There are "; echo $retrive->numberofposts(); echo " posts on this blog.</div>
+<a class='nounderline' href='/console/posts/new' alt='Create New Post' title='Create New Post'><div class='createnewpost'>Create New Post</div></a>";
+echo dbsearchbar();
+	echo '<h1 style="display:table; margin:0;">No search terms entered, please enter search terms above.</h1>';
+	$haserror = true;
+  } else {
+$author3 = implode(':',$author2[0]);
+$replace = array('author:', '"', ',');
+$author4 = str_replace($replace, '', $author3);
+if ($retrive->userid($author4) == true){
+$author5 = $retrive->userid($author4);
+$author = " AND content_author = '".$author5."' ";
+} else {
+	echo consolemenu();
+echo "<div id='page'>
+<div class='center'>There are "; echo $retrive->numberofposts(); echo " posts on this blog.</div>
+<a class='nounderline' href='/console/posts/new' alt='Create New Post' title='Create New Post'><div class='createnewpost'>Create New Post</div></a>";
+echo dbsearchbar();
+	echo '<h1 style="display:table; margin:0;">Author not found.</h1>';
+	$haserror = true;
+}
+  }
+  } else if (preg_match('/^author:.*/', $url) or preg_match('/, author:.*/', $url)){
+preg_match_all ('/author:.*/im', $url, $author2);
 if(empty($author2[0])){
 	echo consolemenu();
 echo "<div id='page'>
@@ -164,49 +220,53 @@ $postsperpage = $postsperpageinit->fetch_assoc();
 $ppp = $postsperpage['postsperpage'];
 
 $result2 = $global->sqlquery("SELECT COUNT(*) FROM dd_content");
-$row2 = $result2->fetch_row(); 
-$total_records = $row2[0];
-$total_pages = ceil($total_records / $ppp);
-	if ($page == '1'){
-	$count = $page;
-	}
-	else {
-	$count = $page + $ppp - '1';
-	}
+        if ($page == '1'){
+        $count = $page;
+        }
+        else {
+        $count = $page + $ppp - '1';
+        }
 
-	if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; }; 
-$start_from = ($page-1) * $ppp; 
+        if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
+$start_from = ($page-1) * $ppp;
+
 	
 $contentquery = " content_title LIKE ('".$searchterm5."') OR content_description LIKE ('".$searchterm5."')";
 $query = "SELECT * FROM dd_content WHERE".$contentquery.$category.$tags.$date.$author." ORDER BY content_date DESC LIMIT $start_from, $ppp;";
+$query2 = "SELECT COUNT(*) FROM dd_content WHERE".$contentquery.$category.$tags.$date.$author.";";
 
 if(trim($searchterm5) === ''){
 $contentquery = "";
-$query = "SELECT * FROM dd_content WHERE".$contentquery.$category.$tags.$date.$author." ORDER BY content_date DESC LIMIT $start_from, $ppp;";
+$query = "SELECT * FROM dd_content WHERE".$category.$tags.$date.$author." ORDER BY content_date DESC LIMIT $start_from, $ppp;";
+$query2 = "SELECT COUNT(*) FROM dd_content WHERE".$category.$tags.$date.$author.";";
 $pos = strpos($query, 'AND');
 if ($pos !== false) {
     $newstring = substr_replace($query, '', '31', strlen('AND'));
 }
+$pos = strpos($query2, 'AND');
+if ($pos !== false) {
+    $newstring2 = substr_replace($query2, '', 38, strlen('AND'));
+}
 $results = $global->sqlquery($newstring);
+$results2 = $global->sqlquery($newstring2);
 }
 else {
 $results = $global->sqlquery($query);
+$results2 = $global->sqlquery($query2);
 }
-$searchresultnumbers = $results->num_rows;
+$searchresultnumbers = $results2->fetch_row();
+$total_records = $searchresultnumbers[0];
+$total_pages = ceil($total_records2 / $ppp);
+if ($results->num_rows > 0) {
 echo consolemenu();
 echo "<div id='page'>
 <div class='center'>There are "; echo $retrive->numberofposts(); echo " posts on this blog.</div>
 <a class='nounderline' href='/console/posts/new' alt='Create New Post' title='Create New Post'><div class='createnewpost'>Create New Post</div></a>";
 
-	if ($page == '1'){
-	$count = $page;
-	}
-	else {
-	$count = $page + $ppp - '1';
-	}
 echo dbsearchbar();
 echo '<div class="contentpostscroll">';
 if ($results->num_rows > 0) {
+echo '<h1 style="display:table; margin:0;">There are '.number_format($searchresultnumbers[0]).' search results.</h1>';
     // output data of each row
     while($row = $results->fetch_assoc()) {
 	echo '<div class="postbox">';
@@ -228,6 +288,15 @@ if ($results->num_rows > 0) {
 	$count++;
 	}
 }
-echo pagebar($page, $total_pages, $ppp, '5');
+echo pagebar($page, $total_pages, $ppp, '5' , '1');
 		echo '</div>';
+} else {
+		echo consolemenu();
+echo "<div id='page'>
+<div class='center'>There are "; echo $retrive->numberofposts(); echo " posts on this blog.</div>
+<a class='nounderline' href='/console/posts/new' alt='Create New Post' title='Create New Post'><div class='createnewpost'>Create New Post</div></a>";
+echo dbsearchbar();
+    echo "<h1>No search results found.</h1>";
+	echo '</div>';
+}
 }
