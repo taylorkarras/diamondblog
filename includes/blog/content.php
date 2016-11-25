51e0d2561ecbac9adf3ebf5c675cca6af92a9495
taylorkarras <taylorkarras@gmail.com>
2016-11-25 12:44:17 -0800

    v1.1 update

--- includes/blog/content.php
+++ includes/blog/content.php
@@ -9,14 +9,57 @@ $ppp = $postsperpage['postsperpage'];
 if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; }; 
 $start_from = ($page-1) * $ppp; 
 
-$result = $global->sqlquery("SELECT * FROM dd_content ORDER BY content_date DESC LIMIT $start_from, $ppp;");
+$result = $global->sqlquery("SELECT * FROM dd_content WHERE content_pinned = '0' ORDER BY content_date DESC LIMIT $start_from, $ppp;");
 $result2 = $global->sqlquery("SELECT COUNT(*) FROM dd_content");
 $row2 = $result2->fetch_row(); 
 $total_records = $row2[0];
 $total_pages = ceil($total_records / $ppp);
 
+$pinned = $global->sqlquery("SELECT * FROM dd_content WHERE content_pinned = '1' OR content_pinned = '2';");
+
 $check = new DB_check;
 pluginClass::hook( "content_top" );
+echo '<div class="addthis_inline_follow_toolbox"></div>';
+// Pinned post //
+if ($pinned->num_rows > 0) {
+    // output data of each row
+echo '<div id="pinned">';
+echo '<h2>Pinned</h2>';
+    while($row = $pinned->fetch_assoc()) {
+		$date=date_create($row['content_date']);
+		// Comments
+		echo '<div class="contentcomment"><a class="contentcomment" href="'; echo $row['content_permalink']; echo '#comments" title="Comment & share!" alt="Comment & share!">Comments (';echo $check->retrieve_comment_count($row['content_id']); echo')'; echo'<a/></div>';
+		// Title
+        echo '<a href="';echo $row['content_permalink']; echo '" class="contenttitle" title="';echo $row['content_title']; echo '" alt="';echo $row['content_title']; echo '"><div class="contenttitle">';echo $row['content_title']; echo '</div></a>';
+		// Date
+		echo '<div class="contentdate">Posted on '.date_format($date, $postsperpage['date_format']." ".$postsperpage['time_format']).' by '.$retrive->realname($row['content_author']).'</div>';
+		// Post
+		echo '<div class="contentpost">'; echo $row['content_embedcode'];
+		echo '<br />';
+		echo $row['content_summary'];
+		if (strpos($row['content_summary'], "...")){
+		echo '<p><a class="readmore" href="'.$row['content_permalink'].'" title="';echo $row['content_title']; echo '" alt="';echo $row['content_title']; echo '">(read more)</a></p>';
+		}
+		echo '</div>';
+		// Category
+		echo '<div class="contentcategory">Categorized under: <a href="/category?name=';
+		$catlowcase = strtolower($row['content_category']);
+		echo str_replace(" ", "_", $catlowcase);
+		echo '" alt="'; echo $row['content_category']; echo '" title="'; echo $row['content_category']; echo'">'; echo $row['content_category']; echo '</div></a>';
+		// Tags
+		echo '<div class="contenttags">Tags: ';
+		$tags = explode (", ", $row['content_tags']);
+		foreach ($tags as $tag) {
+			echo '<a href="/tag?name=';
+		$taglowcase = strtolower($tag);
+		echo str_replace(" ", "_", $taglowcase);
+		echo '" alt="'; echo $tag; echo '" title="'; echo $tag; echo'">'; echo $tag; echo '</a> ';
+		}
+		echo '</div>';
+    }
+}
+echo '</div>';
+
 echo '<div class="contentpostscroll">';
 if ($result->num_rows > 0) {
     // output data of each row
@@ -31,7 +74,11 @@ if ($result->num_rows > 0) {
 		// Post
 		echo '<div class="contentpost">'; echo $row['content_embedcode'];
 		echo '<br />';
-		echo $row['content_summary']; echo '</div>';
+				echo $row['content_summary'];
+		if (strpos($row['content_summary'], "...")){
+		echo '<p><a class="readmore" href="'.$row['content_permalink'].'" title="';echo $row['content_title']; echo '" alt="';echo $row['content_title']; echo '">(read more)</a></p>';
+		}
+		echo '</div>';
 		// Category
 		echo '<div class="contentcategory">Categorized under: <a href="/category?name=';
 		$catlowcase = strtolower($row['content_category']);
