754d67a871b396287168ca56cfe63086cddb3624
taylorkarras <taylorkarras@gmail.com>
2016-11-28 01:47:29 -0800

    minor update

--- includes/checks.php
+++ includes/checks.php
@@ -17,6 +17,20 @@ public function ifbanned() {
 	}
 }
 
+private function ReverseIPOctets($inputip){
+$ipoc = explode(".",$inputip);
+return $ipoc[3].".".$ipoc[2].".".$ipoc[1].".".$ipoc[0];
+}
+
+public function istor(){
+if (gethostbyname($this->ReverseIPOctets($_SERVER['REMOTE_ADDR']).".".$_SERVER['SERVER_PORT'].".".$this->ReverseIPOctets($_SERVER['SERVER_ADDR']).".ip-port.exitlist.torproject.org")=="127.0.0.2") {
+return true;
+} else {
+return false;
+} 
+}
+
+
 public function ifvotedpositive($id) {
 		$global = new DB_global;
 	
