3ab8d4fd2c6f2873c5b6ef0e70561889e0efdeb3
taylorkarras <taylorkarras@gmail.com>
2016-12-05 14:50:25 -0800

    Update index.php

--- includes/console/index.php
+++ includes/console/index.php
@@ -8,7 +8,7 @@
 <link rel="stylesheet" media="screen and (max-width : 535px)" href="https://<?php echo $_SERVER['HTTP_HOST']; ?>/includes/console/styles/style-540px.css">
 <link rel="stylesheet" media="screen and (max-width : 482px)" href="https://<?php echo $_SERVER['HTTP_HOST']; ?>/includes/console/styles/style-480px.css">
 <title>DiamondBlog Console<?php if (defined('POSTPEND')){
-	echo ' - '.POSTPEND.'';
+	echo ' - '.POSTPEND.' '.PAGE.'';
 } ?></title>
 <script src="https://<?php echo $_SERVER['HTTP_HOST']; ?>/includes/console/scripts/jquery-2.2.3.min.js"></script>
 <script src="https://<?php echo $_SERVER['HTTP_HOST']; ?>/includes/console/scripts/ckeditor/ckeditor.js"></script>
@@ -18,6 +18,7 @@
 <script src="https://<?php echo $_SERVER['HTTP_HOST']; ?>/includes/console/scripts/jquery.autocomplete.js"></script>
 <script src="https://<?php echo $_SERVER['HTTP_HOST']; ?>/includes/console/scripts/jquery.mousewheel.min.js"></script>
 <script src="https://<?php echo $_SERVER['HTTP_HOST']; ?>/includes/console/scripts/jquery.jscroll.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <script>
     var CKEDITOR_BASEPATH = '/scripts/ckeditor/';
 </script>
@@ -26,6 +27,5 @@
 <body>
 <?php phrouteresponse()?>
 <script src="https://<?php echo $_SERVER['HTTP_HOST']; ?>/includes/console/scripts/console-func.js"></script>
-
 </body>
 </html>
