408b26c8620a95525a2e080603bc051057e2dac7
taylorkarras <taylorkarras@gmail.com>
2016-12-24 17:20:17 -0800

    Create reports.php

---
+++ includes/console/pages/reports.php
@@ -0,0 +1,98 @@
+<?php
+$retrive = new DB_retrival;
+if ($retrive->isLoggedIn() == true){
+if (isset($_GET['reportid']))
+{
+$global = new DB_global;
+$result = $global->sqlquery("SELECT * FROM dd_reports WHERE report_id = '".$_GET['reportid']."'");
+$result2 = $result->fetch_assoc();
+$comment = $global->sqlquery("SELECT * FROM dd_comments WHERE comment_id = '".$result2['report_commentid']."'");
+$comment2 = $comment->fetch_assoc();
+
+echo consolemenu();
+echo "<div id='page'>";
+echo "<div class='center'>The specified report is provided below.</div>";
+
+	echo '<div class="postbox">';
+	echo '<div class="postinfobox">';
+	echo '<div class="posttitle">';
+	echo $result2['report_name']; echo' ('.$result2['report_ip'].') ('.$result2['report_email'].')';
+	echo '</div>';
+	echo '<div class="postcategory">'.$result2['report_text'].'</div>';
+	echo '</div>';
+	echo '</div>';
+
+	echo "<br><div class='center'>The comment reported is provided below.</div>";
+	
+		echo '<div class="postbox">';
+	echo '<div class="postinfobox">';
+	echo '<div class="posttitle">';
+	echo $comment2['comment_username']; echo' ('.$comment2['comment_ip'].') ('.$comment2['comment_email'].')';
+	echo '</div>';
+	echo '<div class="postcategory">'.$comment2['comment_content'].'</div>';
+	echo '</div>';
+	echo '</div>';
+	
+	echo "<br><div class='center'>What would you like to do.</div>";
+	echo '<br><div class="center"><a href="/console/posts/comments/delete?commentid='.$result2['report_commentid'].'" title="Delete Comment" alt="Delete Comment">Delete Comment</a> | <a href="/console/posts/comments/unreport?id='.$_GET['reportid'].'&commentid='.$result2['report_commentid'].'" alt="Unreport" title="Unreport">Unreport</a></div>';
+	
+echo '</div>';
+
+} else {
+$_SESSION['referral_url']['comments'] = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+echo consolemenu();
+echo "<div id='page'>";
+echo "<div class='center'>There are "; echo number_format($retrive->numberofreports()); echo " reports on this blog.</div>";
+
+$global = new DB_global;
+
+		$ss1 = $global->sqlquery("SELECT * FROM dd_settings");
+		$ss2 = $ss1->fetch_assoc();
+
+$ppp = $ss2['postsperpage'];
+
+if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; }; 
+$start_from = ($page-1) * $ppp; 
+
+if ($page > '1'){
+define ("PAGE", ' (Page '.$_GET['page'].')');
+}
+
+$result = $global->sqlquery("SELECT * FROM dd_reports ORDER BY report_id DESC LIMIT $start_from, $ppp;");
+$result2 = $global->sqlquery("SELECT COUNT(*) FROM dd_reports");
+$row2 = $result2->fetch_row(); 
+$total_records = $row2[0];
+$total_pages = ceil($total_records / $ppp);
+	if ($page == '1'){
+	$count = $page;
+	}
+	else {
+	$count = $page + $start_from - $page + '1';
+	}
+echo '<div class="contentpostscroll">';
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+	echo '<div class="postbox">';
+	echo '<div class="postoptions">';
+	echo '<a href="/console/reports?reportid='.$row['report_id'].'" title="View Detailed Information" alt="View Detailed Information">View Detailed Information</a>';
+	echo '</div>';
+	echo '<div class="postnumber">';
+	echo $count;
+	echo '</div>';
+	echo '<div class="postinfobox">';
+	echo '<div class="posttitle">';
+	echo $row['report_name']; echo' ('.$row['report_ip'].') ('.$row['report_email'].')';
+	echo '</div>';
+	echo '<div class="postcategory">'.$row['report_text'].'</div>';
+	echo '</div>';
+	echo '</div>';
+	$count++;
+	}
+}
+echo pagebar($page, $total_pages, $ppp, '5');
+		echo '</div>';
+		}}else {
+	header('Location: https://'.$_SERVER['HTTP_HOST'].'/console');
+}
+?>
