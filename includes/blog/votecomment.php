2121ca3bc2ea93fa727829a1801c4be43e57e9a6
taylorkarras <taylorkarras@gmail.com>
2016-12-24 17:22:14 -0800

    Update votecomment.php

--- includes/blog/votecomment.php
+++ includes/blog/votecomment.php
@@ -15,38 +15,38 @@ else {
 	$positivevoteinit = $positivevote->fetch_array();
 	$voteip = $positivevoteinit['cvote_ip'];
 	
-if ($_GET["vote"] == "positive" && $positivevoteinit['cvote_positive'] == '0' && $positivevoteinit['cvote_negative'] == '1' && $voteip > 0) {
+if ($_GET["vote"] == "positive" && $positivevoteinit['cvote_positive'] == '0' && $positivevoteinit['cvote_negative'] == '1' && $voteip == $IP) {
 	
 	$global->sqlquery("UPDATE `dd_votes` SET `cvote_voted` = '1', `cvote_positive` = '1', `cvote_negative` = '0' WHERE `cvote_ip` = '".$IP."' AND `cvote_commentid` = '".$totalvotesinit['comment_id']."';");
 	
 }
 
-else if ($_GET["vote"] == "positive" && $positivevoteinit['cvote_positive'] == '1' && $positivevoteinit['cvote_negative'] == '0' && $voteip > 0) {
+else if ($_GET["vote"] == "positive" && $positivevoteinit['cvote_positive'] == '1' && $positivevoteinit['cvote_negative'] == '0' && $voteip == $IP) {
 	
 	$global->sqlquery("DELETE FROM `dd_votes` WHERE `cvote_ip` = '".$IP."' AND `cvote_commentid` = '".$totalvotesinit['comment_id']."';");
 	
 }
 		
-else if ($_GET["vote"] == "positive" && empty($voteip)) {
+else if ($_GET["vote"] == "positive" && $voteip == NULL) {
 	
-	$global->sqlquery("INSERT INTO `dd_votes` (`cvote_ip`, `cvote_commentid`, `cvote_p_id`, `cvote_voted`, `cvote_positive`, `cvote_negative`) VALUES ('".$_SERVER['REMOTE_ADDR']."', '".$totalvotesinit['comment_id']."', '".$_GET["postid"]."', '1', '1', '0')");
+	$global->sqlquery("INSERT INTO `dd_votes` (`cvote_id`, `cvote_ip`, `cvote_commentid`, `cvote_p_id`, `cvote_voted`, `cvote_positive`, `cvote_negative`) VALUES (NULL, '".$_SERVER['REMOTE_ADDR']."', '".$totalvotesinit['comment_id']."', '".$_GET["postid"]."', '1', '1', '0')");
 	}
 
-if ($_GET["vote"] == "negative" && $positivevoteinit['cvote_positive'] == '1' && $positivevoteinit['cvote_negative'] == '0' && $voteip > 0) {
+if ($_GET["vote"] == "negative" && $positivevoteinit['cvote_positive'] == '1' && $positivevoteinit['cvote_negative'] == '0' && $voteip == $IP) {
 	
 	$global->sqlquery("UPDATE `dd_votes` SET `cvote_voted` = '1', `cvote_positive` = '0', `cvote_negative` = '1' WHERE `cvote_ip` = '".$IP."' AND `cvote_commentid` = '".$totalvotesinit['comment_id']."';");
 	
 }
 
-else if ($_GET["vote"] == "negative" && $positivevoteinit['cvote_positive'] == '0' && $positivevoteinit['cvote_negative'] == '1' && $voteip > 0) {
+else if ($_GET["vote"] == "negative" && $positivevoteinit['cvote_positive'] == '0' && $positivevoteinit['cvote_negative'] == '1' && $voteip == $IP) {
 	
 	$global->sqlquery("DELETE FROM `dd_votes` WHERE `cvote_ip` = '".$IP."' AND `cvote_commentid` = '".$totalvotesinit['comment_id']."';");
 
 }
 		
-else if ($_GET["vote"] == "negative" && empty($voteip)) {
+else if ($_GET["vote"] == "negative" && $voteip == NULL) {
 	
-	$global->sqlquery("INSERT INTO `dd_votes` (`cvote_ip`, `cvote_commentid`, `cvote_p_id`,  `cvote_voted`, `cvote_positive`, `cvote_negative`) VALUES ('".$_SERVER['REMOTE_ADDR']."', '".$totalvotesinit['comment_id']."', '".$_GET["postid"]."', '1', '0', '1')");
+	$global->sqlquery("INSERT INTO `dd_votes` (`cvote_id`, `cvote_ip`, `cvote_commentid`, `cvote_p_id`,  `cvote_voted`, `cvote_positive`, `cvote_negative`) VALUES (NULL, '".$_SERVER['REMOTE_ADDR']."', '".$totalvotesinit['comment_id']."', '".$_GET["postid"]."', '1', '0', '1')");
 	
 }
 
