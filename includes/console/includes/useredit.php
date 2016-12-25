1a60634dad1279a069e40142efa0875c35e0c3a2
taylorkarras <taylorkarras@gmail.com>
2016-12-24 18:30:06 -0800

    Update useredit.php

--- includes/console/includes/useredit.php
+++ includes/console/includes/useredit.php
@@ -53,9 +53,75 @@ $userlevel = "`user_isadmin` = '0', `user_iscontributor` = '0', `user_ismod` = '
 	}} else {
 if(isset($usernewpassword1) && isset($usernewpassword2)){
 $userfunc->changePassword($_POST['useridtoedit'],$usernewpassword1,$usernewpassword2);
+
+if($_POST['sendpassword'] == '1'){
+
+if ($retrive->ismailenabled() == true){
+	$mailinit = $global->sqlquery("SELECT * FROM dd_mail");
+	$mail = $mailinit->fetch_assoc();
+	
+$transport = Swift_SmtpTransport::newInstance($mail['mail_server'], 25)
+  ->setUsername($mail['mail_user'])
+  ->setPassword($mail['mail_password'])
+  ;
+} else {
+$transport = Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
+}
+
+$message = Swift_Message::newInstance()->setCharset('iso-8859-2')
+
+  // Give the message a subject
+  ->setSubject(''.sitename().' - Your password has been changed')
+
+  // Set the From address with an associative array
+  ->setFrom(array('no-reply@'.$_SERVER['HTTP_HOST'].'' => sitename()))
+
+  // Set the To addresses with an associative array
+  ->setTo($useremail)
+
+  // Give it a body
+  ->setBody('Hi,
+	
+	Your password has been changed.
+	
+	Your new password is ('.$_POST['$usernewpassword1'].').
+	
+	This is an automatically generated message, do not respond to it. Go ahead and sign in with your credentials at '.$_SERVER['HTTP_HOST'].'/console.
+	
+	Happy blogging!
+	-'.sitename().'')
+
+  // And optionally an alternative body
+  ->addPart('<p>Hi,</p>
+	
+	<p>Your password has been changed.</p>
+	
+	<p>Your new password is ('.$_POST['$usernewpassword1'].').</p>
+	
+	<p>This is an automatically generated message, do not respond to it. Go ahead and sign in with your credentials at <a href=https://'.$_SERVER['HTTP_HOST'].'/console" title="Console", alt="Console">'.$_SERVER['HTTP_HOST'].'/console</a>.</p>
+	
+	<p>Happy blogging!
+	<br />-'.sitename().'</p>', 'text/html');
+
+$mailer = Swift_Mailer::newInstance($transport);
+
+$mailer->send($message);
+
+}
 }
 		
 $global->sqlquery("UPDATE `dd_users` SET `user_realname` = '".$_POST['userrealname']."', `user_description` = '".$_POST['userdescription']."', `user_subtext` = '".$_POST['usersubtext']."', `user_location` = '".$_POST['userlocation']."', `user_email` = '".$useremail."' ".$userlevel." WHERE `user_id` = '".$_POST['useridtoedit']."'");
+
+if(isset($_POST['closedaccount']) && $_POST['closedaccount'] == '1'){
+
+$global->sqlquery("UPDATE `dd_users` SET `user_closedaccount` = '1' WHERE `user_id` = '".$_POST['useridtoedit']."'");
+
+} else if(isset($_POST['closedaccount']) && $_POST['closedaccount'] == '0'){
+	
+$global->sqlquery("UPDATE `dd_users` SET `user_closedaccount` = '0' WHERE `user_id` = '".$_POST['useridtoedit']."'");
+	
+}
+
 			        		if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) &&  strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
 			
 				$resp = array();
@@ -110,7 +176,7 @@ $userlevel = "'0', '0', '1'";
                 exit;
 	}} else {
 		$passwordencrypt = password_hash($_POST['userpassword'], PASSWORD_DEFAULT);
-		$global->sqlquery("INSERT INTO `dd_users` (`user_id`, `user_username`, `user_realname`, `user_password`, `user_picture`, `user_description`, `user_subtext`, `user_location`, `user_isadmin`, `user_iscontributor`, `user_ismod`, `user_email`, `user_datejoined`) VALUES (NULL, '".$username."', '', '".$passwordencrypt."', '', '', '', '', ".$userlevel.", '".$useremail."', CURRENT_DATE())");
+		$global->sqlquery("INSERT INTO `dd_users` (`user_id`, `user_username`, `user_realname`, `user_password`, `user_picture`, `user_description`, `user_subtext`, `user_location`, `user_isadmin`, `user_iscontributor`, `user_ismod`, `user_closedaccount`, `user_email`, `user_datejoined`) VALUES (NULL, '".$username."', '', '".$passwordencrypt."', '', '', '', '', ".$userlevel.", NULL, '".$useremail."', CURRENT_DATE())");
 
 // Create the message
 if ($retrive->ismailenabled() == true){
