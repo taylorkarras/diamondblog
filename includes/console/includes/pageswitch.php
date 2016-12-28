f2acce79b55f60d574be1bec6fc83ac2c08015a5
taylorkarras <taylorkarras@gmail.com>
2016-12-28 03:08:49 -0800

    Update pageswitch.php

--- includes/console/includes/pageswitch.php
+++ includes/console/includes/pageswitch.php
@@ -9,13 +9,13 @@ $global = new DB_global;
 if ($_GET['position'] == 'on'){
 
 $global->sqlquery("UPDATE `dd_settings` SET `pages_on` = '1', `menu_on` = '1'");
-
+header('Location: https://'.$_SERVER['HTTP_HOST'].'/console/settings/categoriesandpages/2');
 }
 
 if ($_GET['position'] == 'off'){
 
 $global->sqlquery("UPDATE `dd_settings` SET `pages_on` = '0', `menu_on` = '0'");
-
+header('Location: https://'.$_SERVER['HTTP_HOST'].'/console/settings/categoriesandpages/2');
 }
 } else {
  header("HTTP/1.0 403 Forbidden");
